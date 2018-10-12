7fffdfae5074c57adea6e230c14504bf138deb10
EC2 Default User <ec2-user@ip-172-31-34-255.us-east-2.compute.internal>
2018-10-12 12:43:29 +0000

    suspending signup

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,8 +1,27 @@
 class PlayersController < ApplicationController
   
+  def show
+    @player = Player.find(params[:id])
+  end
+  
   def new
+    @player = Player.new
   end
   
   def create
+    @player = Player.new(player_params)
+    if @player.save
+      flash[:success] = "プレイヤーアカウントを作成しました"
+      redirect_to @player
+    else
+      render 'new'
+    end
   end
+
+  private
+
+    def player_params
+      params.require(:player).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
