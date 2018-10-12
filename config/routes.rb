167a5c196f1c8570b63a279799b4f2438b43d819
EC2 Default User <ec2-user@ip-172-31-34-255.us-east-2.compute.internal>
2018-10-12 04:55:32 +0000

    Finish top pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  get 'top_pages/home'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'top_pages#home'
 end
--- test/controllers/top_pages_controller_test.rb
+++ test/controllers/top_pages_controller_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class TopPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @base_title = "Team App"
+  end 
+  
   test "should get home" do
-    get top_pages_home_url
+    get root_url
     assert_response :success
+    assert_select "title", @base_title
   end
 
 end
