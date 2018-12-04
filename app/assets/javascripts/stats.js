97f28cfaf41327619d551b7c8615cea2a44b744e
yano <yano-ryosuke-tg@ynu.jp>
2018-12-04 12:34:01 +0000

    empty tips

--- app/assets/javascripts/stats.js
+++ app/assets/javascripts/stats.js
@@ -1,10 +1,12 @@
 $(document).ready(() => {
-  $('.initial-stats').on('mouseenter', () => {
-    $('.stats-title').show();
-    $('.stats-title').removeClass('hide');
-  });
   
-  $('.stats-title').on('mouseleave', () => {
-    $('.stats-title').hide();
-  });
+  $(document).on({
+    'mouseenter': function(event) {
+      var tips = $(event.currentTarget).attr('data-tips');
+      $(event.currentTarget).html(`<p>${tips}</p>`);
+    },
+    'mouseleave': function(event) {
+      $(event.currentTarget).children('p').empty();
+    }
+  }, '.initial-stats');
 });
