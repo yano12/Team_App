276297e3d30134901a670ca0e7f1df17c883aa7b
yano <yano-ryosuke-tg@ynu.jp>
2018-12-05 13:42:13 +0000

    make stats p tagu

--- app/assets/javascripts/stats.js
+++ app/assets/javascripts/stats.js
@@ -1,12 +1,33 @@
 $(document).ready(() => {
-  
+  /*
   $(document).on({
-    'mouseenter': function(event) {
+      'mouseenter': function(event) {
       var tips = $(event.currentTarget).attr('data-tips');
-      $(event.currentTarget).html(`<p>${tips}</p>`);
+      $(event.currentTarget).text(tips);
+      $(event.currentTarget).css('font-size', '8px');
     },
-    'mouseleave': function(event) {
-      $(event.currentTarget).children('p').empty();
+      'mouseleave': function(event) {
+      var order = $(event.currentTarget).attr('data-order');
+      $(event.currentTarget).text(order);
+      $(event.currentTarget).css('font-size', '12px');
     }
   }, '.initial-stats');
+  */
+  
+  $(document).on({
+      'mouseenter': function(event) {
+      var tips = $(event.currentTarget).attr('data-tips');
+      $('.player-stats').children('p').text(tips);
+      //$('.player-stats').children('p').removeClass('hide');
+      //$('.player-stats').children('p').addClass('stats-relative');
+      //$(event.currentTarget).addClass('absolute');
+    },
+      'mouseleave': function(event) {
+      $('.player-stats').children('p').empty();
+      //$('.player-stats').children('p').addClass('hide');
+      //$('.player-stats').children('p').removeClass('stats-relative');
+      //$(event.currentTarget).removeClass('absolute');
+    }
+  }, '.initial-stats');
+  
 });
